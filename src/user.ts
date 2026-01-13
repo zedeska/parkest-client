ba8e180415edd4ea432e461a36853f973286ede6
capitainekiwi88-glitch <capitainekiwi88@gmail.com>
2026-01-13 18:32:14 +0100

    Update user.ts

--- src/user.ts
+++ src/user.ts
@@ -1,16 +1,15 @@
 import { Login, Register } from "./api";
 
-class User{
-
+class User {
     hauteur: number;
     pmr: boolean;
     dspOnly: boolean;
     elec: boolean;
     free: boolean;
     username: string = "";
-    token:  string = "";
+    token: string = "";
     
-    constructor(hauteur: number, pmr: boolean,elec: boolean,free: boolean, dspOnly: boolean){
+    constructor(hauteur: number, pmr: boolean, elec: boolean, free: boolean, dspOnly: boolean) {
         this.hauteur = hauteur;
         this.pmr = pmr;
         this.elec = elec;
@@ -18,18 +17,23 @@ class User{
         this.dspOnly = dspOnly;
     }
 
-    async login(username: string, password: string): Promise<void>{
-        try {
-            this.token = await Login(username, password);
-            this.username = username;
-        } catch (error) {
-            throw error;
-        }
+    async login(username: string, password: string): Promise<void> {
+    const data = await Login(username, password);
+    this.token = data.token;
+    this.username = username;
+    if (data.params) {
+        this.hauteur = data.params.hauteur;
+        this.pmr = data.params.pmr;
+        this.free = data.params.free;
+        this.elec = data.params.elec;
+        this.dspOnly = data.params.dspOnly;
     }
+}
 
-    async register(username: string, password: string): Promise<void>{
+    async register(username: string, password: string): Promise<void> {
         try {
-            this.token = await Register(username, password);
+            const data = await Register(username, password);
+            this.token = data.token || data;
             this.username = username;
         } catch (error) {
             throw error;
